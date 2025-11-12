ed5ef62eeda1e8286e3e4eca719c6578cdb067ca
nguyenhai13200 <nguyenhoanghai13200@gmail.com>
2025-11-12 16:01:02 +0700

    feat: run "yarn prepare"

---
+++ lib/typescript/src/NativeTestRnLibrary.d.ts
@@ -0,0 +1,8 @@
+import { type TurboModule } from 'react-native';
+export interface Spec extends TurboModule {
+    multiply(a: number, b: number): number;
+    sayHello(): string;
+}
+declare const _default: Spec;
+export default _default;
+//# sourceMappingURL=NativeTestRnLibrary.d.ts.map
---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,3 @@
+export declare function multiply(a: number, b: number): number;
+export declare function sayHello(): string;
+//# sourceMappingURL=index.d.ts.map
