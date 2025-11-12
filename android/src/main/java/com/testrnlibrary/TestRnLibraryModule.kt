b31a97c0ad853b7b0a85a464a202718d94e0224b
nguyenhai13200 <nguyenhoanghai13200@gmail.com>
2025-11-12 09:10:24 +0700

    feat: android native module

--- android/src/main/java/com/testrnlibrary/TestRnLibraryModule.kt
+++ android/src/main/java/com/testrnlibrary/TestRnLibraryModule.kt
@@ -1,11 +1,12 @@
 package com.testrnlibrary
 
+import com.example.mylibrary.Hello
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
 
 @ReactModule(name = TestRnLibraryModule.NAME)
 class TestRnLibraryModule(reactContext: ReactApplicationContext) :
-  NativeTestRnLibrarySpec(reactContext) {
+        NativeTestRnLibrarySpec(reactContext) {
 
   override fun getName(): String {
     return NAME
@@ -15,6 +16,10 @@ class TestRnLibraryModule(reactContext: ReactApplicationContext) :
   // See https://reactnative.dev/docs/native-modules-android
   override fun multiply(a: Double, b: Double): Double {
     return a * b
+  }
+
+  override fun sayHello(): String {
+    return "Hello from Native Android Module! " + Hello().sayHello()
   }
 
   companion object {
